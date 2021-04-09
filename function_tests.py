d0dcc9971c4454081ecaa681dfe1d5b712b0d9f9
shushushucier <2586784545@qq.com>
2021-04-09 14:10:25 +0800

    Basic view now returns minimal HTML

--- function_tests.py
+++ function_tests.py
@@ -1,22 +1,41 @@
 from selenium import webdriver
 import unittest
 
-class  NewVistorTest(object):           # (1)
-	
-	def setUp(self): # (3)
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
 		self.browser = webdriver.Firefox()
 
-	def tearDown(self): # (3)
+	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self): # (2)
-	    # Edith has heard about a cool new online to-do app. She goes
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title) # (4)
-		self.fail('Finish the test!') # (5)
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
-	if __name__ == '__main__': # (6)
-	    unittest.main(warning='ignore') #(7)
+# She is invited to enter a to-do item straight away
+
+# She types "Buy peacock feathers" into a text box(Edith's hobby
+# is trying fly-fishing lures)
+
+# when she hits enter, the page updates, and now the page lists
+# "1: Buy peacock feathers" as an item in a to-do lists
+
+# There is still a text box inviting her to add another item. She
+# enters "Use peacock feathers to make a fly"(Edith is very methodical)
+
+# The page updates again, and now shows both items on her list
+
+# Edith wonders whether the site will remember her list. The she sees
+# that the site has generated a unique URL for her -- there is some
+# explanatory text to that effect.
+
+# She visits that URL - her to-do list is still there.
+
+# Satisfied, she goes back to sleep
+if __name__ == '__main__':
+	unittest.main(warnings = 'ignore')
