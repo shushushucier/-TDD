95d6242dcbd60a6ba239b3d3b327dc660890d8c8
shushushucier <2586784545@qq.com>
2021-04-09 13:39:22 +0800

    First unit test and url mapping, dummy view

--- function_tests.py
+++ function_tests.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class  NewVistorTest(object):           # (1)
+	
+	def setUp(self): # (3)
+		self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+	def tearDown(self): # (3)
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self): # (2)
+	    # Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title) # (4)
+		self.fail('Finish the test!') # (5)
+
+	if __name__ == '__main__': # (6)
+	    unittest.main(warning='ignore') #(7)
